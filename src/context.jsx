a1cfbeef044b37fdc252fddd2fba2724905e6037
Melina Rentzi <MelinaRentzi@users.noreply.github.com>
2024-07-13 18:25:39 +0000

    Axios and meals api

--- src/context.jsx
+++ src/context.jsx
@@ -1,8 +1,24 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
+import axios from "axios";
 
 const AppContext = React.createContext();
 
+const allMealsURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=a";
+const randomMealURL = "https://www.themealdb.com/api/json/v1/1/random.php";
+
 const AppProvider = ({ children }) => {
+  const fetchMeals = async (url) => {
+    try {
+      const { data } = await axios(url);
+      console.log(data);
+    } catch (e) {
+      console.log(e.response);
+    }
+  };
+
+  useEffect(() => {
+    fetchMeals(allMealsURL);
+  }, []);
   return (
     <AppContext.Provider value={{ name: "melina", role: "student" }}>
       {children}
